0b70c6fd7c3d1363539dfca12612f46160567618
Dorian4536 <jb2034360@gmail.com>
2026-03-27 11:17:53 +0100

    problem is vercel changed it

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,8 @@ const [query, setQuery] = useState("book,nature,animals,people");
   initialPageParam: 1,
 
   getNextPageParam: (pages) => {
-    return pages.length + 1;
+    const lastPage = pages[pages.length - 1];
+    return lastPage.total_pages > pages.length ? pages.length + 1 : undefined;
   },
 
   enabled: true
